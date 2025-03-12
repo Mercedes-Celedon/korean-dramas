5cf4d0610e1dbd7e5982c62e583829f830abec9d
Mercedes-Celedon <mercedes.celedon.95@gmail.com>
2025-03-12 09:10:55 -0600

    adding Angular Proyect to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  selector: 'nombre-diferente',
+  imports: [RouterOutlet, CommonModule],
+  /* template: `
+  @if(isLoggedIn){
+    <div><h1>hola</h1></div>
+  }@else{
+    <div><h1>Adiós</h1></div>
+  }
+  @for(lista of lista; track lista.id){
+    <div><h2>{{lista.title}}</h2>
+    <button (click)="greet()">Botón misterioso</button>
+    </div>
+  }`, */
+  templateUrl:'./app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+greet() {
+alert('Holi, soy una ventana');
+}
   title = 'my-first-angular-app';
+  isLoggedIn = false;
+  lista=[{id:1, title:'Elemento 1'},{id:2, title:'Elemento 2'},{id:2, title:'Elemento 3'}];
 }
---
+++ src/app/environments/environmentSupabase.ts
@@ -0,0 +1,5 @@
+export const environmentSupabase = {
+    production: false,
+    supabaseUrl: 'https://nelnlrdrbjfjnqqcpptt.supabase.co',
+    supabaseKey: 'YOUR_SUPABASE_KEY',
+  }
